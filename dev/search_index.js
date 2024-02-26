584ff654af75aa9a49bf657ce10626909fc0ef45
Documenter.jl <documenter@juliadocs.github.io>
2024-02-26 17:16:27 +0000

    build based on b9cfe21

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = RansacDoneRight","category":"page"},{"location":"#RansacDoneRight","page":"Home","title":"RansacDoneRight","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for RansacDoneRight.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [RansacDoneRight]","category":"page"}]
+}
